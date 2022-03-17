8f8a480d2e8dcb2af86e2fdc489482edefb2eb70
Michel Wene <michelwene@hotmail.com>
2022-03-17 14:13:49 -0300

    add component Home and finish styles component Home

--- src/components/SignInButton/indext.tsx
+++ src/components/SignInButton/indext.tsx
@@ -1,3 +1,24 @@
+import styles from "./styles.module.scss";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+
 export function SignInButton() {
-  return <button type="button">Sign in with Github</button>;
+  const userIsLogged = true;
+
+  return userIsLogged ? (
+    <>
+      <button type="button" className={styles.signInButton}>
+        <FaGithub color="#04d361" />
+        Michel Wene
+        <FiX color="#737380" className={styles.closeIcon} />
+      </button>
+    </>
+  ) : (
+    <>
+      <button type="button" className={styles.signInButton}>
+        <FaGithub color="#eba417" />
+        Sign in with Github
+      </button>
+    </>
+  );
 }
