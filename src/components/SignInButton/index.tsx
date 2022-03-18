2505ab76f8d9c89844011668ae22054988fd3732
Michel Wene <michelwene@hotmail.com>
2022-03-18 10:55:57 -0300

    add environment variables from github,  configure GithubProvider and connect auth Github

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,11 +1,12 @@
+import { signIn, useSession } from "next-auth/react";
 import styles from "./styles.module.scss";
 import { FaGithub } from "react-icons/fa";
 import { FiX } from "react-icons/fi";
 
 export function SignInButton() {
-  const userIsLogged = true;
+  const { data: session } = useSession();
 
-  return userIsLogged ? (
+  return session ? (
     <>
       <button type="button" className={styles.signInButton}>
         <FaGithub color="#04d361" />
@@ -15,7 +16,11 @@ export function SignInButton() {
     </>
   ) : (
     <>
-      <button type="button" className={styles.signInButton}>
+      <button
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signIn("github")}
+      >
         <FaGithub color="#eba417" />
         Sign in with Github
       </button>
