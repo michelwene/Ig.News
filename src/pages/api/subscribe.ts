eaaa4a551f156511c31374a92e9eba4bce38c20e
Michel Wene <michelwene@hotmail.com>
2022-03-20 09:53:07 -0300

    repair connection with stripe

--- src/pages/api/subscribe.ts
+++ src/pages/api/subscribe.ts
@@ -40,7 +40,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       customerId = stripeCustomer.id;
     }
 
-    const stripeCheckoutSession = stripe.checkout.sessions.create({
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
       customer: customerId,
       payment_method_types: ["card"],
       billing_address_collection: "required",
@@ -53,10 +53,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
       mode: "subscription",
       allow_promotion_codes: true,
       success_url: process.env.STRIPE_SUCCESS_URL,
-      cancel_url: process.env.STRYPE_CANCEL_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
     });
 
-    return res.status(200).json({ sessionId: (await stripeCheckoutSession).id });
+    return res.status(200).json({ sessionId: stripeCheckoutSession.id });
   } else {
     res.setHeader("Allow", "POST");
     res.status(405).end("Method not allowed");
