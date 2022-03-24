0f32f605a53d56d1dd24fc904f6e399c2df87ab3
Michel Wene <michelwene@hotmail.com>
2022-03-24 16:57:07 -0300

    validate session subscription

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -8,7 +8,11 @@ export function SignInButton() {
 
   return session ? (
     <>
-      <button type="button" className={styles.signInButton} onClick={() => signOut()}>
+      <button
+        type="button"
+        className={styles.signInButton}
+        onClick={() => signOut()}
+      >
         <FaGithub color="#04d361" />
         {session.user.name}
         <FiX color="#737380" className={styles.closeIcon} />
